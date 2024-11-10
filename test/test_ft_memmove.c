caa2e1c9e2c2b475e9cdc03ecae8a3fccdb629e1
Lucas Codeur <l.dadamopro@gmail.com>
2024-11-10 15:17:42 +0100

    add new test like if dest is null or size equal to 0

--- test/test_ft_memmove.c
+++ test/test_ft_memmove.c
@@ -16,21 +16,21 @@ void	test_1_same_size(void)
 	printf(BLUE"\n------------------------- END --------------------------\n\n"RESET);
 }
 
-void	test_2_dest_have_smaller_size_than_src(void)
-{
-	char	str1[] = "Luff";
-	char	str2[] = "Zoro!";
+// void	test_2_dest_have_smaller_size_than_src(void)
+// {
+// 	char	str1[] = "Luff";
+// 	char	str2[] = "Zoro!";
 
-	printf(BLUE"------------------- TEST FT_MEMMOVE -------------------\n\n"RESET);
-	printf("TEST 2 : DEST HAVE SMALLER SIZE THAN SRC ");
-	printf(YELLOW"Before memmove : \n\n"RESET);
-	printf(RED"DEST : %s\n\n"RESET, str1);
-	memmove(str1, str2, sizeof(str2));
-	printf(YELLOW"After memmove : \n\n"RESET);
-	printf(GREEN"DEST : %s"RESET, str1);
-	printf("\n");
-	printf(BLUE"\n------------------------- END --------------------------\n"RESET);
-}
+// 	printf(BLUE"------------------- TEST FT_MEMMOVE -------------------\n\n"RESET);
+// 	printf("TEST 2 : DEST HAVE SMALLER SIZE THAN SRC ");
+// 	printf(YELLOW"Before memmove : \n\n"RESET);
+// 	printf(RED"DEST : %s\n\n"RESET, str1);
+// 	memmove(str1, str2, sizeof(str2));
+// 	printf(YELLOW"After memmove : \n\n"RESET);
+// 	printf(GREEN"DEST : %s"RESET, str1);
+// 	printf("\n");
+// 	printf(BLUE"\n------------------------- END --------------------------\n"RESET);
+// }
 
 void	test_3_bigger_size_for_dest(void)
 {
@@ -77,15 +77,46 @@ void	test_5_if_size_is_0(void)
 	printf(BLUE"\n------------------------- END --------------------------\n\n"RESET);
 }
 
-void	test_6_if_the_size_is_bigger_of_the_strings(void)
+// void	test_6_if_the_size_is_bigger_of_the_strings(void)
+// {
+// 	char	str1[] = "Luffy";
+
+// 	printf(BLUE"------------------- TEST FT_MEMMOVE -------------------\n\n"RESET);
+// 	printf("TEST 6 : IF SIZE IS BIGGER THE TWO STRINGS \n\n");
+// 	printf(YELLOW"Before memmove : \n\n"RESET);
+// 	printf(RED"DEST : %s\n\n"RESET, str1);
+// 	memmove(str1, str1 + 1, 10);
+// 	printf(YELLOW"After memmove : \n\n"RESET);
+// 	printf(GREEN"DEST : %s"RESET, str1);
+// 	printf("\n");
+// 	printf(BLUE"\n------------------------- END --------------------------\n\n"RESET);
+// }
+
+// void	test_7_if_src_is_null(void)
+// {
+// 	char	str1[] = "";
+
+// 	printf(BLUE"------------------- TEST FT_MEMMOVE -------------------\n\n"RESET);
+// 	printf("TEST 7 : IF SRC IS NULL \n\n");
+// 	printf(YELLOW"Before memmove : \n\n"RESET);
+// 	printf(RED"DEST : %s\n\n"RESET, str1);
+// 	memmove(str1, str1 + 1, 1);
+// 	printf(YELLOW"After memmove : \n\n"RESET);
+// 	printf(GREEN"DEST : %s"RESET, str1);
+// 	printf("\n");
+// 	printf(BLUE"\n------------------------- END --------------------------\n\n"RESET);
+// }
+
+void	test_8_if_dest_is_null(void)
 {
-	char	str1[] = "Luffy";
+	char	str1[] = "";
+	char	str2[] = "zoro";
 
 	printf(BLUE"------------------- TEST FT_MEMMOVE -------------------\n\n"RESET);
-	printf("TEST 6 : IF SIZE IS BIGGER THE TWO STRINGS \n\n");
+	printf("TEST 8: IF DEST IS NULL \n\n");
 	printf(YELLOW"Before memmove : \n\n"RESET);
 	printf(RED"DEST : %s\n\n"RESET, str1);
-	memmove(str1, str1 + 1, 10);
+	memmove(str1, str2, sizeof(str2));
 	printf(YELLOW"After memmove : \n\n"RESET);
 	printf(GREEN"DEST : %s"RESET, str1);
 	printf("\n");
@@ -100,6 +131,8 @@ void	test_ft_memmove(void)
 	// test_4_overlapping();
 	// test_5_if_size_is_0();
 	// test_6_if_the_size_is_bigger_of_the_strings();
+	// test_7_if_src_is_null();
+	// test_8_if_dest_is_null();
 }
 
 // if dest is more smaller than src I don't have to handle it, I have to let the overflow
@@ -109,3 +142,5 @@ void	test_ft_memmove(void)
 	//(erase the current date than memmove must copy)
 // If the size is 0 you must have no change
 // If the size of the size we have to copy is bigger than the two string, there is an overflow and I haven't to handle it
+// If src is null the compilator left an error saying we try to reading 1 bytes from a region of size 0
+// If dest is null the compilator said I try to writing 5 bytes into a region of size 1 overlows the destination
